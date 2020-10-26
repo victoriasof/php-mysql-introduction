a693308fdf16dd2e58e8b9c96ee3fd652f28a382
Victoria <victoria.sofianidou@gmail.com>
2020-10-26 17:02:33 +0100

    trying to use pdo

--- insert.php
+++ insert.php
@@ -12,10 +12,30 @@ error_reporting(E_ALL);
 $connection = new Connection();
 $pdo = $connection->openConnection();
 
-// heck connection
+//$pdo = openConnection();
 
+if(!empty($_POST['first_name']) && !empty($_POST['last_name'])) {
 
+    if (empty($_POST['id'])) {//switched if statement
+        $handle = $pdo->prepare('INSERT INTO user (first_name, last_name) VALUES (:first_name, :last_name)');
+        $message = 'Your record has been added';
+    } else {
+        $handle = $pdo->prepare('UPDATE user SET first_name = :first_name, last_name = :last_name, WHERE id = :id');
+        $handle->bindValue(':id', $_POST['id']);
+        $message = 'Your record has been updated';
+    }
+}
+/*
+I AM MIXING PDO AND SQL. ALWAYS USE PDO
 
+$firstName = $lastName = $email ="";
+
+$sql = "INSERT INTO student (first_name, last_name, email)
+VALUES ('$firstName','$lastName','$email')";
+
+if ($pdo->query($sql) === TRUE) {
+    echo "New record created successfully";
+}
 
 $sql = "SELECT id, first_name, last_name, email, created_at FROM student"; //get from table not from database
 $result = $pdo->query($sql);
@@ -23,8 +43,7 @@ $result = $pdo->query($sql);
     while ($row = $result->fetchAll()) {
         echo "id: " . $row["id"] . " - Name: " . $row["first_name"] . " " . $row["last_name"] . " " . $row["email"] . " " . $row["created_at"];"<br>";
     }
-
-
+*/
 
 $firstName = $lastName= $email = "";
 $firstNameErr = $lastNameErr = $emailErr = "";
