7990d2b6119e88185e12ddc0a017f5f7a68c8ec5
Victoria <victoria.sofianidou@gmail.com>
2020-10-28 15:46:24 +0100

    getting errors again when trying to insert info in the database

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -4,8 +4,34 @@ Class RegisterController {
 
     public function render(){
 
+        $firstName = $lastName = $email = "";
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $firstName = test_input($_POST['firstName']);
+            $lastName = test_input($_POST['lastName']);
+            $email = test_input($_POST['email']);
+
+        }
+
+
+        $pdo = Connection::openConnection();
+        $statement = $pdo->prepare("INSERT INTO students(first_name, last_name, email, password)VALUES(:firstName, :lastName, :email)");
+
+        $statement->bindValue(":fistName", $firstName);
+        $statement->bindValue(":lastName", $lastName);
+        $statement->bindValue(":email", $email);
+
+        $statement->execute();
+
+
+
+
+
+
+        require 'View/register.php';
 
     }
 
 }
+
--- View/register.php
+++ View/register.php
@@ -1,1 +1,48 @@
-<?php
+<?php require 'View/includes/header.php'; ?>
+
+<div class="container">
+
+
+    <form method="post">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="firstname">First name:</label>
+                    <input class="form-control" type="text" id="firstName" name="firstName" value="<?php echo $firstName ?>">
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="lastname">Last name:</label>
+                    <input class="form-control" type="text" id="lastName" name="lastName" value="<?php echo $lastName ?>">
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input class="form-control" type="password" id="password" name="password">
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="passwordConfirm">Password Confirm:</label>
+                    <input class="form-control" type="password" id="passwordConfirm" name="passwordConfirm">
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input class="form-control" type="text" id="email" name="email" value="<?php echo $email ?>">
+
+                </div>
+            </div>
+        </div>
+        <input class="btn btn-primary" type="submit" value="Submit">
+    </form>
+</div>
+
+<?php require 'View/includes/footer.php' ?>
