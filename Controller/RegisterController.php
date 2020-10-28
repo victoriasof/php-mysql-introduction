69c0f4913f5abb25fbe03497762609236036c30a
Victoria <victoria.sofianidou@gmail.com>
2020-10-28 16:01:33 +0100

    errors again

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -5,28 +5,43 @@ Class RegisterController {
     public function render(){
 
         $firstName = $lastName = $email = "";
+        $firstNameErrorMessage = $lastNameErrorMessage = $emailErrorMessage = $passwordErrorMessage = "";
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $firstName = test_input($_POST['firstName']);
             $lastName = test_input($_POST['lastName']);
             $email = test_input($_POST['email']);
+            $password = test_input($_POST['email']);
 
+        }
+
+        if (empty($firstName)){
+            $firstNameErrorMessage = 'First name is required';
+        }
+
+        if (empty($lastNameName)){
+            $lastNameErrorMessage = 'Lastname is required';
+        }
+
+        if (empty($email)){
+            $emailErrorMessage = 'Email is required';
+        }
+
+        if (empty($password)){
+            $passwordErrorMessage = 'Password is required';
         }
 
 
         $pdo = Connection::openConnection();
-        $statement = $pdo->prepare("INSERT INTO students(first_name, last_name, email, password)VALUES(:firstName, :lastName, :email)");
+        $statement = $pdo->prepare("INSERT INTO students(first_name, last_name, email, password)VALUES(:firstName, :lastName, :email, :password)");
 
         $statement->bindValue(":fistName", $firstName);
         $statement->bindValue(":lastName", $lastName);
         $statement->bindValue(":email", $email);
+        $statement->bindValue(":password", $password);
 
         $statement->execute();
-
-
-
-
 
 
         require 'View/register.php';
