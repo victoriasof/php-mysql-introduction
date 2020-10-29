3093c4eeda2370625671323dbefece9eb37ff418
Victoria <victoria.sofianidou@gmail.com>
2020-10-29 14:43:23 +0100

    fixed an error, another one appeared

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -18,64 +18,69 @@ Class RegisterController {
             $hashedConfirm =password_hash($passwordConfirm, PASSWORD_DEFAULT);
             $authorization = new Auth();
 
+
+            if (empty($firstName)){
+                $firstNameErrorMessage = 'First name is required';
+            }
+
+            if (empty($lastName)){
+                $lastNameErrorMessage = 'Lastname is required';
+            }
+
+            if (empty($email)){
+                $emailErrorMessage = 'Email is required';
+            }
+
+            if (empty($password)){
+                $passwordErrorMessage = 'Password is required';
+            }
+
+            if(empty($passwordConfirm)){
+                $passwordConfirmErrorMessage = 'Password confirmation is required';
+            }
+
+            if($password !== $passwordConfirm){
+
+                $passwordMatchError = "Password with confirm are not equal";
+            }
+
+            if(empty($firstNameErrorMessage) && empty($lastNameErrorMessage)&&empty($emailErrorMessage)&&empty($passwordErrorMessage)&&empty($passwordConfirmErrorMessage)&&empty($passwordMatchError)){
+                //if the register form is valid and no errors insert a new student in the student table
+                $pdo = Connection::openConnection();
+
+                //$connection = new Connection();
+                //$pdo =  $connection->openConnection();
+
+                $statement = $pdo->prepare("INSERT INTO student(first_name, last_name, email, password)VALUES(:firstName, :lastName, :email, :password)");
+
+                $statement->bindValue(":fistName", $firstName);
+                $statement->bindValue(":lastName", $lastName);
+                $statement->bindValue(":email", $email);
+                $statement->bindValue(":password", $hashedPassword);
+
+                $statement->execute();
+
+                //Get and integer of how many students got inserted (1always)
+                $rowsChanged = $statement->rowCount();
+                //Get the id of the new student created
+                $id = $pdo->lastInsertId();
+
+
+                $_SESSION['firstName'] = $firstName;
+                $_SESSION['lastName'] = $lastName;
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $hashedPassword;
+
+                Header("Location: ?user=$id");
+
+            }
+
         }
-
-        if (empty($firstName)){
-            $firstNameErrorMessage = 'First name is required';
-        }
-
-        if (empty($lastNameName)){
-            $lastNameErrorMessage = 'Lastname is required';
-        }
-
-        if (empty($email)){
-            $emailErrorMessage = 'Email is required';
-        }
-
-        if (empty($password)){
-            $passwordErrorMessage = 'Password is required';
-        }
-
-        if(empty($passwordConfirm)){
-            $passwordConfirmErrorMessage = 'Password confirmation is required';
-        }
-
-        if($password !== $passwordConfirm){
-
-            $passwordMatchError = "Password with confirm are not equal";
-        }
-
-        if(empty($firstNameErrorMessage) && empty($lastNameErrorMessage)&&empty($emailErrorMessage)&&empty($passwordConfirmErrorMessage)&&empty($passwordMatchError)){
-
-            $connection = new Connection();
-            $pdo =  $connection->openConnection();
-
-            $statement = $pdo->prepare("INSERT INTO student(first_name, last_name, email, password)VALUES(:firstName, :lastName, :email, :password)");
-
-            $statement->bindValue(":fistName", $firstName);
-            $statement->bindValue(":lastName", $lastName);
-            $statement->bindValue(":email", $email);
-            $statement->bindValue(":password", $password);
-
-            $statement->execute();
-
-            //Get and integer of how many students got inserted (1always)
-            $rowsChanged = $statement->rowCount();
-            //Get the id of the new student created
-            $id = $pdo->lastInsertId();
-
-        }
-
-        $_SESSION['firstName'] = $firstName;
-        $_SESSION['lastName'] = $lastName;
-        $_SESSION['email'] = $email;
-        $_SESSION['password'] = $hashedPassword;
-
-        Header("Location: ?user=$id");
 
         require 'View/register.php';
 
     }
 
 }
+
 
