4a8f665bbbe7188e527d171f8df17bdadca5bcdc
Victoria <victoria.sofianidou@gmail.com>
2020-10-29 17:01:50 +0100

    few last changes

--- View/login.php
+++ View/login.php
@@ -1,20 +1,38 @@
 <?php require 'View/includes/header.php' ?>
 
-<div class="container">
-    <div id="form">
-<!--        <div class="alert alert-danger">-->
-<!--            Invalid credentials-->
-<!--        </div>-->
-        <form method="post" action="">
-            <label for="email">Email</label><br>
-            <input class="form-control" type="text" name="email" id="email" value="<?php echo $email ?>">
+    <div class="container my-5">
+        <?php
+        if (!empty($loginError)) {
+            echo "
+                <div class=\"alert alert-danger\">
+                    Invalid credentials
+                </div>
+            ";
+        }elseif (isset($_SESSION['isAuthenticated']) && $_SESSION['isAuthenticated']) {
+            echo "
+                <div class=\"alert alert-success\">
+                    Logged in successfully
+                </div>
+            ";
+        }
+        ?>
 
-            <label for="password">Password</label><br>
-            <input class="form-control" type="password" name="password" id="password">
-
-            <input class="btn btn-primary" type="submit" value="Submit">
-        </form>
+        <h1>Login form</h1>
+        <div id="form">
+            <form method="post" action="">
+                <div class="form-group">
+                    <label for="email">Email</label><br>
+                    <input class="form-control" type="text" name="email" id="email" value="<?php echo $email ?>">
+                    <p class="form-error-message"><?php echo $emailErrorMessage ?></p>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label><br>
+                    <input class="form-control" type="password" name="password" id="password">
+                    <p class="form-error-message"><?php echo $passwordErrorMessage ?></p>
+                </div>
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
+        </div>
     </div>
-</div>
 
 <?php require 'View/includes/footer.php' ?>
--- View/profile.php
+++ View/profile.php
@@ -1,12 +1,21 @@
 <?php require 'View/includes/header.php' ?>
 
-<div class="container">
-    <h1>Profile</h1>
-    <img src="<?php echo $img ?>" style="width: 200px;">
-    <p>Firstname: <?php echo $currentStudent->getFirstName() ?></p>
-    <p>Lastname: <?php echo $currentStudent->getLastName() ?></p>
-    <p>Email: <?php echo $currentStudent->getEmail() ?></p>
-    <p>Created At: <?php echo $currentStudent->getCreatedAt() ?></p>
-</div>
+    <div class="container">
+        <h1>Profile</h1>
+        <img src="<?php echo $img ?>" style="width: 200px;">
+        <p>Firstname: <?php echo $currentStudent->getFirstName() ?></p>
+        <p>Lastname: <?php echo $currentStudent->getLastName() ?></p>
+        <p>Email: <?php echo $currentStudent->getEmail() ?></p>
+        <p>Created At: <?php echo $currentStudent->getCreatedAt() ?></p>
+        <?php
+        if ($canEdit) {
+            echo "
+            <div class=\"profile-actions\">
+                <a href=\"?user=$id&edit=1\" class=\"btn btn-primary\">Edit profile</a>
+            </div>
+        ";
+        }
+        ?>
+    </div>
 
 <?php require 'View/includes/footer.php' ?>
